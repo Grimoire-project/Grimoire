2b6a16d7e65644e8f9922b1d3a218e11e8bbad89
KarlHarnois <karl@goDynamo.com>
2017-10-14 16:49:12 -0400

    Implement |> for throwable functions

--- Tests/GrimoireKitTests/OperatorsSpec.swift
+++ Tests/GrimoireKitTests/OperatorsSpec.swift
@@ -9,8 +9,16 @@ final class OperatorsSpec: QuickSpec {
         return x + 1
       }
 
+      func decrement(x: Int) throws -> Int {
+        return x - 1
+      }
+
       it("pipes a value into a function") {
         expect(3 |> increment) == 4
+      }
+
+      it("works with throwable functions") {
+        expect { try 5 |> decrement } == 4
       }
     }
   }
