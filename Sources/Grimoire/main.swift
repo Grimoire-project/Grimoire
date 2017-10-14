11bb722170ab848e6be70535c8320c5dbc4fc58e
KarlHarnois <karl@goDynamo.com>
2017-10-14 16:49:45 -0400

    Print localized description in main.swift
    Some foundation errors are very verbose but most of them have user
    friendly localizedDescriptions.

--- Sources/Grimoire/main.swift
+++ Sources/Grimoire/main.swift
@@ -3,5 +3,5 @@ import GrimoireKit
 do {
   try GrimoireKit.command(args: CommandLine.arguments).execute()
 } catch {
-  print("Something wrong happened:", error)
+  print(error.localizedDescription)
 }
